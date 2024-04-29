2f35497aea16a0ef096a88308786e4a6d7288383
false-fox <154926234+false-fox@users.noreply.github.com>
2024-04-28 19:08:55 -0700

    add sitemap, playform/compress and robotstxt, add sister repo to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # falsefox's personal blog
 
 Check out the post [here](https://falsefox.dev/posts/2024/a-new-blog/). This repository uses [astro](https://astro.build/) and [utterances](https://utteranc.es/) for tech.
+
+[sister repo](https://github.com/false-fox/blog-comments) for utterances comments
 ## License
 Copyright, all rights reserved. (too lazy to deal with what FOSS license is right for my blog no one will read anyway)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import expressiveCode from "astro-expressive-code";
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import sitemap from "@astrojs/sitemap";
+import playformCompress from "@playform/compress";
+
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [expressiveCode({
     themes: ['dark-plus'],
+    site: "https://falsefox.dev",
     plugins: [pluginLineNumbers()],
     styleOverrides: {
       codeFontFamily: "var(--codeFontFamily)",
-      codeFontSize: "var(--codeFontSize)", 
+      codeFontSize: "var(--codeFontSize)"
     }
-  })]
+  }), sitemap(), playformCompress(), robotsTxt()]
 });
